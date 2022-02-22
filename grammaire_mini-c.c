0e5001a065a2342b668c2180f6d5baf5d065816c
hamzabvb <kh_korbaa@esi.dz>
2022-02-22 22:32:41 +0100

    suppression e-production

--- grammaire_mini-c.c
+++ grammaire_mini-c.c
@@ -1,9 +1,15 @@
 Function → Type identifier (ArgList ) CompoundStmt
-ArgList → Arg | ArgList , Arg
+
+ArgList → Type identifier | ArgList , Arg   // supp production unitaire
+
 Arg → Type identifier
+
 Declaration → Type IdentList ;
+
 Type → int | float
+
 IdentList → identifier , IdentList | identifier
+
 Stmt → ForStmt
 		| WhileStmt
 		| Expr ;
@@ -13,15 +19,29 @@ Stmt → ForStmt
 		| ;
 
 ForStmt → for ( Expr ; OptExpr ; OptExpr ) Stmt
-OptExpr → Expr | ε
+		|for ( Expr ; ; OptExpr ) Stmt 		// supp ε-productions
+		|for ( Expr ; OptExpr ; ) Stmt
+		|for ( Expr ; ; ) Stmt
+
+
 WhileStmt → while ( Expr ) Stmt
+
 IfStmt → if ( Expr ) Stmt ElsePart
+
 ElsePart → else Stmt | ε
+
 CompoundStmt → { StmtList }
+
 StmtList → StmtList Stmt | ε
+
 Expr → identifier = Expr | Rvalue
+
 Rvalue → Rvalue Compare Mag | Mag
+
 Compare → == | < | > | <= | >= | !=
+
 Mag → Mag + Term | Mag - Term | Term
+
 Term → Term * Factor | Term / Factor | Factor
+
 Factor → ( Expr ) | - Factor | + Factor | identifier | number
